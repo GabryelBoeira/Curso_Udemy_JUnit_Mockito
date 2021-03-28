e8f31e7ee3c4719a1c89dcf61d90a8f32b47fa35
Gabryel Boeira <gabryel392@gmail.com>
2021-03-28 13:00:42 -0300

    Utilizando novas Annotation
    Utilizando outras formas de validação e definições no JUnit

--- Calculadora/src/com/br/gabryel/calculadora/CalculadoraTest.java
+++ Calculadora/src/com/br/gabryel/calculadora/CalculadoraTest.java
@@ -1,19 +1,42 @@
 package com.br.gabryel.calculadora;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
 
 	@Test
-	void testSomar() {
+	public void testSomar() {
+		
+		Calculadora calculadora = new Calculadora();
+		int resultado = calculadora.somar(5, 5);
+		assertEquals(10, resultado, "Sucesso");
+	}
+	
+	@Ignore
+	public void testSomarSemAnnotation() {
 		
 		Calculadora calculadora = new Calculadora();
 		int resultadoEsperado = 10;
 		
 		int resultado = calculadora.somar(5, 5);
+				
+		assertEquals(resultadoEsperado, resultado);
+	}
+	
+	@Test
+	public void testSomarNumeroNegativo() {
+		
+		Calculadora calculadora = new Calculadora();
+		int resultadoEsperado = -10;
+		
+		int resultado = calculadora.somar(-4, -6);
 		
 		assertTrue(resultadoEsperado == resultado, "Sucesso");
 	}
+	
+	
 }
--- Calculadora/src/module-info.java
+++ Calculadora/src/module-info.java
@@ -1,6 +1,7 @@
 module Calculadora {
 	requires org.junit.jupiter.api;
-	
+	requires junit;
+		
 	/**
 	 *	@author Gabryel 
 	 * 	Aplicação simples com teste criado apartir do framework JUnit 
